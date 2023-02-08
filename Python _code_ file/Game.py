85a2f55ea1b543110141165f544712dc04436b66
PMCBlackstar <66456514+PMCBlackstar@users.noreply.github.com>
2023-02-08 10:17:26 -0500

    Add files via upload

---
+++ Python _code_ file/Game.py
@@ -0,0 +1,16 @@
+# Write your code here :
+smile = Actor("smile")
+smile.pos = 100, 50
+
+WIDTH = 500
+
+HEIGHT = smile.height + 30
+
+
+def draw():
+    screen.fill((255, 0, 0))
+    smile.draw()
+
+
+def update():
+    smile.left + smile.left + 2
