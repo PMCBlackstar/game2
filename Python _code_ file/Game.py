e18cb3a15bfbbd03e8b9e97f02d08e0e711a267f
PMCBlackstar <66456514+PMCBlackstar@users.noreply.github.com>
2023-02-10 10:22:09 -0500

    Update Game.py

--- Python _code_ file/Game.py
+++ Python _code_ file/Game.py
@@ -1,4 +1,4 @@
-# Write your code here :
+# Write your code here :-)
 smile = Actor("smile")
 smile.pos = 100, 50
 
@@ -14,3 +14,15 @@ def draw():
 
 def update():
     smile.left + smile.left + 2
+    if smile.left > WIDTH:
+        smile.right = 0
+
+
+def on_mouse_down(pos):
+    if smile.collidepoint(pos):
+        print("YIPP!")
+    else:
+        print("You missed me!")
+    if smile.collidepoint(pos):
+        sounds.hit.play()
+        smile.image = "smile clicked"
